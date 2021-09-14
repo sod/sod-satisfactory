7ce27091be38c8da8f4115f3e6691768262db523
Alexander von Weiss <sod@users.noreply.github.com>
2021-09-14 17:21:31 +0200

    add bootstrap4 and purgecss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<router-outlet></router-outlet>`,
+    template: `<div class="container"><router-outlet></router-outlet></div>`,
 })
 export class AppComponent {}
