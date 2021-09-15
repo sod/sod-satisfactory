46be1cc396e2ae2440e982fd07f3c3a48fabdc00
Alexander von Weiss <sod@users.noreply.github.com>
2021-09-15 23:41:18 +0200

    sort built items to the bottom and fade them out

--- src/app/planner/component/default/default.component.ts
+++ src/app/planner/component/default/default.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {Store} from '@ngrx/store';
+import {sortBy} from 'lodash-es';
+import {map} from 'rxjs/operators';
 import {TrackByService} from 'src/app/shared/service/track-by-service';
 import {GlobalState} from '../../../shared/store/global-state';
 import {selectPlannerEditProduction, selectProductions} from '../../../shared/store/planner/planner.selectors';
@@ -10,7 +12,7 @@ import {selectPlannerEditProduction, selectProductions} from '../../../shared/st
     styleUrls: ['./default.component.scss'],
 })
 export class DefaultComponent {
-    public productions$ = this.store.select(selectProductions);
+    public productions$ = this.store.select(selectProductions).pipe(map((productions) => sortBy(productions, 'built')));
     public edit$ = this.store.select(selectPlannerEditProduction);
 
     constructor(private store: Store<GlobalState>, public trackByService: TrackByService) {}
