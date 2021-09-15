4399873b53d34093b152215543559ab48efcab3a
Alexander von Weiss <sod@users.noreply.github.com>
2021-09-15 14:01:56 +0200

    move position of the todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<div class="container"><router-outlet></router-outlet></div>`,
+    templateUrl: './app.component.html',
 })
 export class AppComponent {}
