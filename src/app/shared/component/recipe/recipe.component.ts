c5bb2f8915149d45a5e316de7a682c77754ad6c7
Alexander von Weiss <sod@users.noreply.github.com>
2022-06-26 19:24:53 +0200

    add eslint

--- src/app/shared/component/recipe/recipe.component.ts
+++ src/app/shared/component/recipe/recipe.component.ts
@@ -1,12 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-recipe',
     templateUrl: './recipe.component.html',
     styleUrls: ['./recipe.component.scss'],
 })
-export class RecipeComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
-}
+export class RecipeComponent {}
